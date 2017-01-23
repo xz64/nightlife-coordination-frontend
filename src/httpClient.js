68fe5cae860edf9531d14fcf64f10226126e3f4f
S K <xz64@users.noreply.github.com>
2017-01-23 23:30:01 +0000

    feat: add 401 response interceptor

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: '/api',
 });
+
+instance.interceptors.response.use(
+  null,
+  (error) => {
+    if (error.response.status === 401) {
+      window.location.href = '/api/login/github';
+    }
+  },
+);
+
+export default instance;
